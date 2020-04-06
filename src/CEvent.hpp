1a085b5e9b1aff828d8a71c38964f0a34ae6d756
Andy Somogyi <andy.somogyi@gmail.com>
2020-04-06 17:28:42 -0400

    updates

---
+++ src/CEvent.hpp
@@ -0,0 +1,15 @@
+/*
+ * CEvent.hpp
+ *
+ *  Created on: Mar 30, 2020
+ *      Author: andy
+ */
+
+#ifndef EXTERN_CARBON_SRC_CEVENT_HPP_
+#define EXTERN_CARBON_SRC_CEVENT_HPP_
+
+class CEvent
+{
+};
+
+#endif /* EXTERN_CARBON_SRC_CEVENT_HPP_ */
