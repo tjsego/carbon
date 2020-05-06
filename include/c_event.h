23c01d50a6cf5b0958a66261976716f9062f11f7
Andy Somogyi <andy.somogyi@gmail.com>
2020-05-06 02:30:08 -0400

    update

--- include/c_event.h
+++ include/c_event.h
@@ -8,6 +8,23 @@
 #ifndef EXTERN_CARBON_INCLUDE_C_EVENT_H_
 #define EXTERN_CARBON_INCLUDE_C_EVENT_H_
 
+#include <c_port.h>
+#include <Python.h>
+
+/**
+ * Basic opaque Event object type (it's actually a PyObject)
+ */
+CAPI_STRUCT(CEvent);
+
+
+CEvent *CEvent_New();
+
+HRESULT CEvent_Invoke(PyObject *args, PyObject *kw_args);
+
+HRESULT CEvent_AddDelegate(PyObject *delegate);
+
+HRESULT CEvent_RemoveDelegate(PyObject *delegate);
+
 
 
 
